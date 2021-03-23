39383f0c21464288cada909b94bae20bb99dc546
lubomir.yonchev <infra1515@gmail.com>
2021-03-23 19:35:31 +0200

    icons added, image loaded from local

--- i_am_rich_flutter/lib/main.dart
+++ i_am_rich_flutter/lib/main.dart
@@ -3,15 +3,12 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
       home: Scaffold(
+          backgroundColor: Colors.blueGrey,
           appBar: AppBar(
             centerTitle: true,
             backgroundColor: Colors.blue,
             title: Text("I AM RICH!"),
           ),
-          body: Center(
-              child: Image(
-                  image: NetworkImage(
-                      "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/"
-                      "Image_created_with_a_mobile_phone.png/"
-                      "1200px-Image_created_with_a_mobile_phone.png"))))));
+          body:
+              Center(child: Image(image: AssetImage('images/diamond.png'))))));
 }
